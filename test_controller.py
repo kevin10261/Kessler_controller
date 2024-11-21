4684fcfbdb7e658409762b9da1512fcfca0a430b
Kevin Yu <112964418+kevin10261@users.noreply.github.com>
2024-11-21 11:41:33 -0700

    Add files via upload

---
+++ test_controller.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+# Copyright © 2022 Thales. All Rights Reserved.
+# NOTICE: This file is subject to the license agreement defined in file 'LICENSE', which is part of
+# this source code package.
+
+from kesslergame import KesslerController
+from typing import Dict, Tuple
+import skfuzzy as fuzz
+
+class TestController(KesslerController):
+    def __init__(self):
+        """
+        Any variables or initialization desired for the controller can be set up here
+        """
+        self.eval_frames = 0
+
+    def actions(self, ship_state: Dict, game_state: Dict) -> Tuple[float, float, bool]:
+        """
+        Method processed each time step by this controller to determine what control actions to take
+
+        Arguments:
+            ship_state (dict): contains state information for your own ship
+            game_state (dict): contains state information for all objects in the game
+
+        Returns:
+            float: thrust control value
+            float: turn-rate control value
+            bool: fire control value. Shoots if true
+            bool: mine deployment control value. Lays mine if true
+        """
+
+        thrust = 0
+        turn_rate = 90
+        fire = True
+        self.eval_frames +=1
+        drop_mine = True
+
+        return thrust, turn_rate, fire , drop_mine
+
+    @property
+    def name(self) -> str:
+        """
+        Simple property used for naming controllers such that it can be displayed in the graphics engine
+
+        Returns:
+            str: name of this controller
+        """
+        return "Test Controller"
+
+    # @property
+    # def custom_sprite_path(self) -> str:
+    #     return "Neo.png"
